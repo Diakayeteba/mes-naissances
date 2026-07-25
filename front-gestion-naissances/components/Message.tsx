f5ca8550700839c2fc9d52dfe4667af534253058
Madou DIAKAYETEBA <madoudiakayeteba@gmail.com>
2026-07-26 00:30:17 +0200

    affichage conditionnel (?)

--- front-gestion-naissances/components/Message.tsx
+++ front-gestion-naissances/components/Message.tsx
@@ -1,11 +1,17 @@
 type Props= {
     firstName : string;
     lastName : string;
+    email?: string;
+    adress?: string;
+    phone?: number;
 };
 function Message(props:Props){
     return (
         <p>
             Bonjour {props.firstName} {props.lastName}
+            {props?.email && ( <span>{props.email}</span>)}
+            {props?.adress && ( <span>{props.adress}</span>)}
+            {props?.phone ? ( <span>{props.phone}</span>):null}
         </p>  
     );
 }
--- front-gestion-naissances/src/App.tsx
+++ front-gestion-naissances/src/App.tsx
@@ -4,8 +4,9 @@ import Message from "../components/Message";
 function App() {
   return (
     <section>
-       <Message firstName="Madou" lastName="DIAKAYETEBA"/> 
-       <Message firstName="AwouKa" lastName="IT Solutions"/> 
+       <Message firstName="Madou" lastName="DIAKAYETEBA" email="madoudiakayeteba@gmail.com"/> 
+       <Message firstName="AwouKa" lastName="IT Solutions" phone={759159183}/> 
+       <Message firstName="InG" lastName="Dev"/> 
        <Message firstName="InG" lastName="Dev"/> 
        
        
